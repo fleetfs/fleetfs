4e8724128523e5c7fe9f3f1a6b9fa329695ea666
Christopher Berner <christopherberner@gmail.com>
2019-01-20 22:23:26 -0800

    Make data storage directory and port configurable

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod fleet;
 extern crate bytes;
 extern crate futures;
 extern crate hyper;
+extern crate clap;
 
 use futures::future;
 use futures::Stream;
@@ -28,16 +29,19 @@ use futures::future::ok;
 use std::io::SeekFrom;
 use std::io::Seek;
 use std::fs::OpenOptions;
+use clap::App;
+use clap::Arg;
+use clap::ArgMatches;
 
 
 type BoxFuture = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
 
-fn truncate(req: Request<Body>) -> BoxFuture {
+fn truncate(req: Request<Body>, data_dir: String) -> BoxFuture {
     let response = req.into_body()
         .concat2()
         .map(move |chunk| {
             let bytes = chunk.bytes();
-            let path = Path::new("junk/junk.txt");
+            let path = Path::new(&data_dir).join("junk.txt");
             let display = path.display();
 
             let mut file = match File::create(&path) {
@@ -53,13 +57,13 @@ fn truncate(req: Request<Body>) -> BoxFuture {
     Box::new(response)
 }
 
-fn write(req: Request<Body>) -> BoxFuture {
+fn write(req: Request<Body>, data_dir: String) -> BoxFuture {
     let offset: u64 = req.uri().path()[1..].parse().unwrap();
     let response = req.into_body()
         .concat2()
         .map(move |chunk| {
         let bytes = chunk.bytes();
-        let path = Path::new("junk/junk.txt");
+        let path = Path::new(&data_dir).join("junk.txt");
         let display = path.display();
 
         let mut file = match OpenOptions::new()
@@ -88,12 +92,12 @@ fn write(req: Request<Body>) -> BoxFuture {
     Box::new(response)
 }
 
-fn read(req: Request<Body>) -> BoxFuture {
+fn read(req: Request<Body>, data_dir: String) -> BoxFuture {
     println!("Reading file");
     let response = req.into_body()
         .concat2()
-        .map(|chunk| {
-            let contents = fs::read_to_string("junk/junk.txt")
+        .map(move |chunk| {
+            let contents = fs::read_to_string(Path::new(&data_dir).join("junk.txt"))
                 .expect("Something went wrong reading the file");
 
             Response::new(Body::from(contents))
@@ -102,18 +106,18 @@ fn read(req: Request<Body>) -> BoxFuture {
     Box::new(response)
 }
 
-fn handler(req: Request<Body>, state: &AtomicUsize) -> BoxFuture {
+fn handler(req: Request<Body>, state: &AtomicUsize, data_dir: String) -> BoxFuture {
     let mut response = Response::new(Body::empty());
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
-            return read(req);
+            return read(req, data_dir);
         },
         (&Method::POST, "/truncate") => {
-            return truncate(req);
+            return truncate(req, data_dir);
         },
         (&Method::POST, _) => {
-            return write(req);
+            return write(req, data_dir);
         },
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
@@ -124,14 +128,53 @@ fn handler(req: Request<Body>, state: &AtomicUsize) -> BoxFuture {
 }
 
 fn main() {
+    let matches = App::new("FleetFS")
+        .version("0.0.0")
+        .author("Christopher Berner")
+        .arg(Arg::with_name("port")
+            .long("port")
+            .value_name("PORT")
+            .default_value("3000")
+            .help("Set server port")
+            .takes_value(true))
+        .arg(Arg::with_name("data-dir")
+            .long("data-dir")
+            .value_name("DIR")
+            .default_value("/tmp/fleetfs")
+            .help("Set local directory used to store data")
+            .takes_value(true))
+        .get_matches();
+
+    let port: u16 = matches.value_of("port").unwrap_or_default().parse().unwrap();
+    let data_dir: String = matches.value_of("data-dir").unwrap_or_default().to_string();
+
+    let path = Path::new(&data_dir).join("junk.txt");
+    let display = path.display();
+
+    match fs::create_dir_all(&data_dir) {
+        Err(why) => panic!("Couldn't create storage dir: {}", why.description()),
+        Ok(_) => ()
+
+    };
+    let mut file = match OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(&path) {
+        Err(why) => panic!("couldn't create {}: {}",
+                           display,
+                           why.description()),
+        Ok(file) => file,
+    };
+
     println!("Starting");
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let addr = ([127, 0, 0, 1], port).into();
 
     let counter = Arc::new(AtomicUsize::new(0));
 
     let new_service = move || {
         let counter2 = counter.clone();
-        service_fn(move |req| handler(req, &counter2))
+        let data_dir2 = data_dir.clone();
+        service_fn(move |req| handler(req, &counter2, data_dir2.clone()))
     };
 
     let server = Server::bind(&addr)
