c3a0825bd525bfb9e157f413e1a22f69a5f02708
Christopher Berner <christopherberner@gmail.com>
2019-04-05 22:08:58 -0700

    Reduce file size used in benchmark

--- bench.sh
+++ bench.sh
@@ -23,13 +23,13 @@ sleep 2
 echo "mounting at $DIR"
 
 echo -e "$GREEN Local disk sequential read $NC"
-fio --name read-test --eta-newline=5s --filename=${BASELINE_DIR}/fio-tempfile.dat --rw=read --size=500m --io_size=10g --blocksize=8k \
+fio --name read-test --eta-newline=5s --filename=${BASELINE_DIR}/fio-tempfile.dat --rw=read --size=500k --io_size=10g --blocksize=8k \
     --ioengine=libaio --fsync=1000 --iodepth=32 --direct=1 --numjobs=1 --runtime=20 --group_reporting
 
 rm -rf ${BASELINE_DIR}
 
 echo -e "$GREEN FleetFS sequential read $NC"
-fio --name read-test --eta-newline=5s --filename=${DIR}/fio-tempfile.dat --rw=read --size=500m --io_size=10g --blocksize=8k \
+fio --name read-test --eta-newline=5s --filename=${DIR}/fio-tempfile.dat --rw=read --size=500k --io_size=10g --blocksize=8k \
     --ioengine=libaio --fsync=1000 --iodepth=32 --direct=1 --numjobs=1 --runtime=20 --group_reporting
 
 rm ${DIR}/fio-tempfile.dat
