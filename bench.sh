c8e85afe04d9d957eac6e1185b939f840cf88138
Christopher Berner <christopherberner@gmail.com>
2019-04-20 23:18:41 -0700

    Switch benchmark to Rust FUSE implementation

--- bench.sh
+++ bench.sh
@@ -17,7 +17,7 @@ DIR2=$(mktemp --directory)
 cargo run --release -- --port 3300 --data-dir $DATA_DIR --peers http://localhost:3301 &
 cargo run --release -- --port 3301 --data-dir $DATA_DIR2 --peers http://localhost:3300 &
 sleep 2
-python client/main.py --server-url http://localhost:3300 --mount-point $DIR &
+cargo run --release -- --server-url http://localhost:3300 --mount-point $DIR &
 sleep 2
 
 echo "mounting at $DIR"
