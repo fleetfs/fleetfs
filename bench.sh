df6add5937fa8cbcf232c0aefb2ad891b9d93eb5
Christopher Berner <christopherberner@gmail.com>
2019-05-22 20:04:36 -0700

    Use connection pooling in PeerClient
    Improves replica read performance by ~30x

--- bench.sh
+++ bench.sh
@@ -20,12 +20,17 @@ cargo run --release -- --port 3301 --data-dir $DATA_DIR2 --peers 127.0.0.1:3300 
 sleep 0.5
 cargo run -- --server-ip-port 127.0.0.1:3300 --get-leader
 cargo run --release -- --server-ip-port 127.0.0.1:3300 --mount-point $DIR &
+cargo run --release -- --server-ip-port 127.0.0.1:3301 --mount-point $DIR2 &
 sleep 0.5
 
 echo "mounting at $DIR"
 
 echo -e "$GREEN FleetFS sequential read $NC"
 fio --name read-test --eta-newline=5s --filename=${DIR}/fio-tempfile.dat --rw=read --size=500k --io_size=10g --blocksize=8k \
+    --ioengine=libaio --fsync=1000 --iodepth=32 --direct=1 --numjobs=1 --runtime=20 --group_reporting
+
+echo -e "$GREEN FleetFS sequential read on second server $NC"
+fio --name read-test --eta-newline=5s --filename=${DIR2}/fio-tempfile.dat --rw=read --size=500k --io_size=10g --blocksize=8k \
     --ioengine=libaio --fsync=1000 --iodepth=32 --direct=1 --numjobs=1 --runtime=20 --group_reporting
 
 rm ${DIR}/fio-tempfile.dat
