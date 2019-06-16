12b1960fb94016e9c78223feadb17fac697ffc74
Christopher Berner <christopherberner@gmail.com>
2019-06-16 16:36:17 -0700

    Refactor FileStorage

--- src/storage/file_storage.rs
+++ src/storage/file_storage.rs
@@ -10,28 +10,24 @@ use log::info;
 use log::warn;
 
 use crate::generated::*;
-use crate::storage::data_storage::BLOCK_SIZE;
+use crate::storage::data_storage::{DataStorage, BLOCK_SIZE};
 use crate::storage::metadata_storage::MetadataStorage;
+use crate::storage_node::LocalContext;
 use crate::utils::{empty_response, into_error_code, ResultResponse};
 
-// Handles one request/response
-pub struct FileStorage<'a> {
-    pub path: String,
+pub struct FileStorage {
     local_data_dir: String,
-    response_buffer: FlatBufferBuilder<'a>,
+    data_storage: DataStorage,
+    metadata_storage: MetadataStorage,
 }
 
-impl<'a> FileStorage<'a> {
-    pub fn new(
-        path: String,
-        local_data_dir: String,
-        builder: FlatBufferBuilder<'a>,
-    ) -> FileStorage<'a> {
+impl FileStorage {
+    pub fn new(node_id: u64, all_node_ids: &[u64], context: &LocalContext) -> FileStorage {
         FileStorage {
             // XXX: hack
-            path: path.trim_start_matches('/').to_string(),
-            local_data_dir,
-            response_buffer: builder,
+            local_data_dir: context.data_dir.clone(),
+            data_storage: DataStorage::new(node_id, all_node_ids, context),
+            metadata_storage: MetadataStorage::new(),
         }
     }
 
@@ -39,27 +35,60 @@ impl<'a> FileStorage<'a> {
         Path::new(&self.local_data_dir).join(path.trim_start_matches('/'))
     }
 
-    pub fn truncate(self, new_length: u64) -> ResultResponse<'a> {
-        let local_path = self.to_local_path(&self.path);
+    // TODO: remove this
+    pub fn get_data_storage(&self) -> &DataStorage {
+        &self.data_storage
+    }
+
+    // TODO: remove this
+    pub fn get_metadata_storage(&self) -> &MetadataStorage {
+        &self.metadata_storage
+    }
+
+    // TODO: this should return an inode
+    pub fn lookup(&self, path: &str) -> String {
+        path.trim_start_matches('/').to_string()
+    }
+
+    pub fn truncate<'a>(
+        &self,
+        path: &str,
+        new_length: u64,
+        builder: FlatBufferBuilder<'a>,
+    ) -> ResultResponse<'a> {
+        self.metadata_storage.truncate(&path, new_length);
+
+        let local_path = self.to_local_path(path);
         let file = File::create(&local_path).expect("Couldn't create file");
         file.set_len(new_length).map_err(into_error_code)?;
 
-        return empty_response(self.response_buffer);
+        return empty_response(builder);
     }
 
-    pub fn mkdir(self, _mode: u16) -> Result<FlatBufferBuilder<'a>, ErrorCode> {
-        assert_ne!(self.path.len(), 0);
+    pub fn mkdir<'a>(
+        &self,
+        path: &str,
+        _mode: u16,
+        builder: FlatBufferBuilder<'a>,
+    ) -> Result<FlatBufferBuilder<'a>, ErrorCode> {
+        assert_ne!(path.len(), 0);
 
-        let path = Path::new(&self.local_data_dir).join(&self.path);
+        self.metadata_storage.mkdir(&path);
+
+        let path = Path::new(&self.local_data_dir).join(path);
         fs::create_dir(path).map_err(into_error_code)?;
 
         // TODO set the mode
 
-        return Ok(self.response_buffer);
+        return Ok(builder);
     }
 
-    pub fn readdir(mut self) -> ResultResponse<'a> {
-        let path = Path::new(&self.local_data_dir).join(&self.path);
+    pub fn readdir<'a>(
+        &self,
+        path: &str,
+        mut builder: FlatBufferBuilder<'a>,
+    ) -> ResultResponse<'a> {
+        let path = Path::new(&self.local_data_dir).join(path);
 
         let mut entries = vec![];
         for entry in fs::read_dir(path).unwrap() {
@@ -72,9 +101,9 @@ impl<'a> FileStorage<'a> {
             } else {
                 unimplemented!()
             };
-            let path = self.response_buffer.create_string(&filename);
+            let path = builder.create_string(&filename);
             let directory_entry = DirectoryEntry::create(
-                &mut self.response_buffer,
+                &mut builder,
                 &DirectoryEntryArgs {
                     path: Some(path),
                     kind: file_type,
@@ -82,49 +111,46 @@ impl<'a> FileStorage<'a> {
             );
             entries.push(directory_entry);
         }
-        self.response_buffer
-            .start_vector::<WIPOffset<DirectoryEntry>>(entries.len());
+        builder.start_vector::<WIPOffset<DirectoryEntry>>(entries.len());
         for &directory_entry in entries.iter() {
-            self.response_buffer.push(directory_entry);
+            builder.push(directory_entry);
         }
-        let entries = self.response_buffer.end_vector(entries.len());
-        let mut builder = DirectoryListingResponseBuilder::new(&mut self.response_buffer);
-        builder.add_entries(entries);
+        let entries = builder.end_vector(entries.len());
+        let mut response_builder = DirectoryListingResponseBuilder::new(&mut builder);
+        response_builder.add_entries(entries);
 
-        let offset = builder.finish().as_union_value();
-        return Ok((
-            self.response_buffer,
-            ResponseType::DirectoryListingResponse,
-            offset,
-        ));
+        let offset = response_builder.finish().as_union_value();
+        return Ok((builder, ResponseType::DirectoryListingResponse, offset));
     }
 
-    pub fn access(
-        self,
+    pub fn access<'a>(
+        &self,
+        path: &str,
         uid: u32,
         gids: &Vector<'_, u32>,
         mut mask: u32,
         metadata_storage: &MetadataStorage,
+        builder: FlatBufferBuilder<'a>,
     ) -> ResultResponse<'a> {
-        if self.path.is_empty() {
+        if path.is_empty() {
             // TODO: currently let everyone access the root
-            return empty_response(self.response_buffer);
+            return empty_response(builder);
         }
 
-        let path = self.to_local_path(&self.path);
-        let metadata = fs::metadata(path).map_err(into_error_code)?;
+        let local_path = self.to_local_path(path);
+        let metadata = fs::metadata(local_path).map_err(into_error_code)?;
 
         // F_OK tests for existence of file
         if mask == libc::F_OK as u32 {
-            return empty_response(self.response_buffer);
+            return empty_response(builder);
         }
 
         // TODO: hacky, because metadata storage only receives changes via chown. Not the original owner
         let file_uid = metadata_storage
-            .get_uid(&self.path)
+            .get_uid(path)
             .unwrap_or_else(|| metadata.st_uid());
         let file_gid = metadata_storage
-            .get_gid(&self.path)
+            .get_gid(path)
             .unwrap_or_else(|| metadata.st_gid());
 
         let mode = metadata.st_mode();
@@ -144,73 +170,75 @@ impl<'a> FileStorage<'a> {
             return Err(ErrorCode::AccessDenied);
         }
 
-        return empty_response(self.response_buffer);
+        return empty_response(builder);
     }
 
-    pub fn getattr(mut self, metadata_storage: &MetadataStorage) -> ResultResponse<'a> {
-        assert_ne!(self.path.len(), 0);
+    pub fn getattr<'a>(
+        &self,
+        path: &str,
+        mut builder: FlatBufferBuilder<'a>,
+    ) -> ResultResponse<'a> {
+        assert_ne!(path.len(), 0);
 
-        let path = Path::new(&self.local_data_dir).join(&self.path);
-        let metadata = fs::metadata(path).map_err(into_error_code)?;
+        let local_path = Path::new(&self.local_data_dir).join(path);
+        let metadata = fs::metadata(local_path).map_err(into_error_code)?;
 
-        let length = metadata_storage.get_length(&self.path).unwrap();
+        let length = self.metadata_storage.get_length(path).unwrap();
 
-        let mut builder = FileMetadataResponseBuilder::new(&mut self.response_buffer);
-        builder.add_size_bytes(length);
-        builder.add_size_blocks(length / BLOCK_SIZE);
+        let mut response_builder = FileMetadataResponseBuilder::new(&mut builder);
+        response_builder.add_size_bytes(length);
+        response_builder.add_size_blocks(length / BLOCK_SIZE);
         let atime = Timestamp::new(metadata.st_atime(), metadata.st_atime_nsec() as i32);
-        builder.add_last_access_time(&atime);
+        response_builder.add_last_access_time(&atime);
         let mtime = Timestamp::new(metadata.st_mtime(), metadata.st_mtime_nsec() as i32);
-        builder.add_last_modified_time(&mtime);
+        response_builder.add_last_modified_time(&mtime);
         let ctime = Timestamp::new(metadata.st_ctime(), metadata.st_ctime_nsec() as i32);
-        builder.add_last_metadata_modified_time(&ctime);
+        response_builder.add_last_metadata_modified_time(&ctime);
         if metadata.is_file() {
-            builder.add_kind(FileKind::File);
+            response_builder.add_kind(FileKind::File);
         } else if metadata.is_dir() {
-            builder.add_kind(FileKind::Directory);
+            response_builder.add_kind(FileKind::Directory);
         } else {
             unimplemented!();
         }
-        builder.add_mode(metadata.st_mode() as u16);
-        builder.add_hard_links(metadata.st_nlink() as u32);
+        response_builder.add_mode(metadata.st_mode() as u16);
+        response_builder.add_hard_links(metadata.st_nlink() as u32);
         // TODO: hacky, because metadata storage only receives changes via chown. Not the original owner
-        if let Some(uid) = metadata_storage.get_uid(&self.path) {
-            builder.add_user_id(uid);
+        if let Some(uid) = self.metadata_storage.get_uid(path) {
+            response_builder.add_user_id(uid);
         } else {
-            builder.add_user_id(metadata.st_uid());
+            response_builder.add_user_id(metadata.st_uid());
         }
-        if let Some(gid) = metadata_storage.get_gid(&self.path) {
-            builder.add_group_id(gid);
+        if let Some(gid) = self.metadata_storage.get_gid(path) {
+            response_builder.add_group_id(gid);
         } else {
-            builder.add_group_id(metadata.st_gid());
+            response_builder.add_group_id(metadata.st_gid());
         }
-        builder.add_device_id(metadata.st_rdev() as u32);
+        response_builder.add_device_id(metadata.st_rdev() as u32);
 
-        let offset = builder.finish().as_union_value();
-        return Ok((
-            self.response_buffer,
-            ResponseType::FileMetadataResponse,
-            offset,
-        ));
+        let offset = response_builder.finish().as_union_value();
+        return Ok((builder, ResponseType::FileMetadataResponse, offset));
     }
 
-    pub fn utimens(
-        self,
+    pub fn utimens<'a>(
+        &self,
+        path: &str,
         uid: u32,
         atime_secs: i64,
         atime_nanos: i32,
         mtime_secs: i64,
         mtime_nanos: i32,
-        metadata_storage: &MetadataStorage,
+        builder: FlatBufferBuilder<'a>,
     ) -> ResultResponse<'a> {
-        assert_ne!(self.path.len(), 0);
+        assert_ne!(path.len(), 0);
 
-        let local_path = self.to_local_path(&self.path);
+        let local_path = self.to_local_path(path);
         let metadata = fs::metadata(&local_path).map_err(into_error_code)?;
 
         // TODO: hacky, because metadata storage only receives changes via chown. Not the original owner
-        let file_uid = metadata_storage
-            .get_uid(&self.path)
+        let file_uid = self
+            .metadata_storage
+            .get_uid(path)
             .unwrap_or_else(|| metadata.st_uid());
 
         // Non-owners are only allowed to change atime & mtime to current:
@@ -229,19 +257,24 @@ impl<'a> FileStorage<'a> {
         )
         .map_err(into_error_code)?;
 
-        return empty_response(self.response_buffer);
+        return empty_response(builder);
     }
 
-    pub fn chmod(self, mode: u32) -> ResultResponse<'a> {
-        assert_ne!(self.path.len(), 0);
+    pub fn chmod<'a>(
+        &self,
+        path: &str,
+        mode: u32,
+        builder: FlatBufferBuilder<'a>,
+    ) -> ResultResponse<'a> {
+        assert_ne!(path.len(), 0);
 
-        let local_path = self.to_local_path(&self.path);
+        let local_path = self.to_local_path(path);
         let c_path =
             CString::new(local_path.to_str().unwrap().as_bytes()).expect("CString creation failed");
         let exit_code;
         unsafe { exit_code = libc::chmod(c_path.as_ptr(), mode) }
         if exit_code == libc::EXIT_SUCCESS {
-            return empty_response(self.response_buffer);
+            return empty_response(builder);
         } else {
             warn!(
                 "chmod failed on {:?}, {} with error {:?}",
@@ -251,64 +284,76 @@ impl<'a> FileStorage<'a> {
         }
     }
 
-    pub fn hardlink(mut self, new_path: &str) -> ResultResponse<'a> {
-        assert_ne!(self.path.len(), 0);
+    pub fn hardlink<'a>(
+        &self,
+        path: &str,
+        new_path: &str,
+        mut builder: FlatBufferBuilder<'a>,
+    ) -> ResultResponse<'a> {
+        assert_ne!(path.len(), 0);
 
-        info!("Hardlinking file: {} to {}", self.path, new_path);
-        let local_path = self.to_local_path(&self.path);
+        self.metadata_storage.hardlink(&path, &new_path);
+
+        info!("Hardlinking file: {} to {}", path, new_path);
+        let local_path = self.to_local_path(path);
         let local_new_path = self.to_local_path(new_path);
         // TODO error handling
         fs::hard_link(&local_path, &local_new_path).expect("hardlink failed");
         let metadata = fs::metadata(local_new_path).map_err(into_error_code)?;
 
-        let mut builder = FileMetadataResponseBuilder::new(&mut self.response_buffer);
-        builder.add_size_bytes(metadata.len());
-        builder.add_size_blocks(metadata.st_blocks());
+        let mut response_builder = FileMetadataResponseBuilder::new(&mut builder);
+        response_builder.add_size_bytes(metadata.len());
+        response_builder.add_size_blocks(metadata.st_blocks());
         let atime = Timestamp::new(metadata.st_atime(), metadata.st_atime_nsec() as i32);
-        builder.add_last_access_time(&atime);
+        response_builder.add_last_access_time(&atime);
         let mtime = Timestamp::new(metadata.st_mtime(), metadata.st_mtime_nsec() as i32);
-        builder.add_last_modified_time(&mtime);
+        response_builder.add_last_modified_time(&mtime);
         let ctime = Timestamp::new(metadata.st_ctime(), metadata.st_ctime_nsec() as i32);
-        builder.add_last_metadata_modified_time(&ctime);
+        response_builder.add_last_metadata_modified_time(&ctime);
         if metadata.is_file() {
-            builder.add_kind(FileKind::File);
+            response_builder.add_kind(FileKind::File);
         } else if metadata.is_dir() {
-            builder.add_kind(FileKind::Directory);
+            response_builder.add_kind(FileKind::Directory);
         } else {
             unimplemented!();
         }
-        builder.add_mode(metadata.st_mode() as u16);
-        builder.add_hard_links(metadata.st_nlink() as u32);
-        builder.add_user_id(metadata.st_uid());
-        builder.add_group_id(metadata.st_gid());
-        builder.add_device_id(metadata.st_rdev() as u32);
+        response_builder.add_mode(metadata.st_mode() as u16);
+        response_builder.add_hard_links(metadata.st_nlink() as u32);
+        response_builder.add_user_id(metadata.st_uid());
+        response_builder.add_group_id(metadata.st_gid());
+        response_builder.add_device_id(metadata.st_rdev() as u32);
 
-        let offset = builder.finish().as_union_value();
-        return Ok((
-            self.response_buffer,
-            ResponseType::FileMetadataResponse,
-            offset,
-        ));
+        let offset = response_builder.finish().as_union_value();
+        return Ok((builder, ResponseType::FileMetadataResponse, offset));
     }
 
-    pub fn rename(self, new_path: &str) -> ResultResponse<'a> {
-        assert_ne!(self.path.len(), 0);
+    pub fn rename<'a>(
+        &self,
+        path: &str,
+        new_path: &str,
+        builder: FlatBufferBuilder<'a>,
+    ) -> ResultResponse<'a> {
+        assert_ne!(path.len(), 0);
 
-        info!("Renaming file: {} to {}", self.path, new_path);
-        let local_path = self.to_local_path(&self.path);
+        self.metadata_storage.rename(&path, &new_path);
+
+        info!("Renaming file: {} to {}", path, new_path);
+        let local_path = self.to_local_path(path);
         let local_new_path = self.to_local_path(new_path);
         fs::rename(local_path, local_new_path).map_err(into_error_code)?;
 
-        return empty_response(self.response_buffer);
+        return empty_response(builder);
     }
 
-    pub fn unlink(self) -> ResultResponse<'a> {
-        assert_ne!(self.path.len(), 0);
+    pub fn unlink<'a>(&self, path: &str, builder: FlatBufferBuilder<'a>) -> ResultResponse<'a> {
+        assert_ne!(path.len(), 0);
+
+        self.metadata_storage.unlink(&path);
 
         info!("Deleting file");
-        let local_path = self.to_local_path(&self.path);
+        let local_path = self.to_local_path(path);
         fs::remove_file(local_path).map_err(into_error_code)?;
 
-        return empty_response(self.response_buffer);
+        return empty_response(builder);
     }
 }
