0bfa7320e926d5b82c9b561d6220972ff571e3b2
Christopher Berner <christopherberner@gmail.com>
2019-06-16 10:07:12 -0700

    Refactor raft_handler into separate handlers module

---
+++ src/handlers/raft_handler.rs
@@ -0,0 +1,91 @@
+use crate::generated::*;
+use crate::raft_manager::RaftManager;
+use crate::utils::{empty_response, finalize_response};
+use flatbuffers::{FlatBufferBuilder, UnionWIPOffset, WIPOffset};
+use futures::future::{ok, result};
+use futures::Future;
+use protobuf::Message as ProtobufMessage;
+use raft::prelude::Message;
+
+pub fn raft_handler<'a, 'b>(
+    request: GenericRequest<'a>,
+    raft_manager: &RaftManager,
+    mut builder: FlatBufferBuilder<'b>,
+) -> impl Future<Item = FlatBufferBuilder<'b>, Error = ErrorCode> {
+    let response: Box<
+        Future<
+                Item = (
+                    FlatBufferBuilder<'b>,
+                    ResponseType,
+                    WIPOffset<UnionWIPOffset>,
+                ),
+                Error = ErrorCode,
+            > + Send,
+    >;
+
+    match request.request_type() {
+        RequestType::FilesystemCheckRequest => unreachable!(),
+        RequestType::FilesystemChecksumRequest => unreachable!(),
+        RequestType::ReadRequest => unreachable!(),
+        RequestType::ReadRawRequest => unreachable!(),
+        RequestType::GetXattrRequest => unreachable!(),
+        RequestType::ListXattrsRequest => unreachable!(),
+        RequestType::SetXattrRequest => unreachable!(),
+        RequestType::RemoveXattrRequest => unreachable!(),
+        RequestType::AccessRequest => unreachable!(),
+        RequestType::HardlinkRequest => unreachable!(),
+        RequestType::RenameRequest => unreachable!(),
+        RequestType::ChmodRequest => unreachable!(),
+        RequestType::ChownRequest => unreachable!(),
+        RequestType::TruncateRequest => unreachable!(),
+        RequestType::FsyncRequest => unreachable!(),
+        RequestType::UnlinkRequest => unreachable!(),
+        RequestType::RmdirRequest => unreachable!(),
+        RequestType::WriteRequest => unreachable!(),
+        RequestType::UtimensRequest => unreachable!(),
+        RequestType::ReaddirRequest => unreachable!(),
+        RequestType::GetattrRequest => unreachable!(),
+        RequestType::MkdirRequest => unreachable!(),
+        RequestType::RaftRequest => {
+            let raft_request = request.request_as_raft_request().unwrap();
+            let mut deserialized_message = Message::new();
+            deserialized_message
+                .merge_from_bytes(raft_request.message())
+                .unwrap();
+            raft_manager
+                .apply_messages(&[deserialized_message])
+                .unwrap();
+            response = Box::new(result(empty_response(builder)));
+        }
+        RequestType::LatestCommitRequest => {
+            let index = raft_manager.get_latest_local_commit();
+            let mut response_builder = LatestCommitResponseBuilder::new(&mut builder);
+            response_builder.add_index(index);
+            let response_offset = response_builder.finish().as_union_value();
+            response = Box::new(ok((
+                builder,
+                ResponseType::LatestCommitResponse,
+                response_offset,
+            )));
+        }
+        RequestType::GetLeaderRequest => {
+            let leader_future = raft_manager
+                .get_leader()
+                .map(move |leader_id| {
+                    let mut response_builder = NodeIdResponseBuilder::new(&mut builder);
+                    response_builder.add_node_id(leader_id);
+                    let response_offset = response_builder.finish().as_union_value();
+                    (builder, ResponseType::NodeIdResponse, response_offset)
+                })
+                .map_err(|_| ErrorCode::Uncategorized);
+
+            response = Box::new(leader_future);
+        }
+        RequestType::NONE => unreachable!(),
+    }
+
+    response.map(|(mut builder, response_type, response_offset)| {
+        finalize_response(&mut builder, response_type, response_offset);
+        builder
+    })
+}
--- src/storage_node.rs
+++ src/storage_node.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::fs;
 
-use flatbuffers::{FlatBufferBuilder, UnionWIPOffset, WIPOffset};
+use flatbuffers::FlatBufferBuilder;
 use futures::Stream;
 use tokio::codec::length_delimited;
 use tokio::net::TcpListener;
@@ -9,101 +9,14 @@ use tokio::prelude::*;
 
 use crate::file_handler::file_request_handler;
 use crate::generated::*;
+use crate::handlers::raft_handler::raft_handler;
 use crate::raft_manager::RaftManager;
-use crate::utils::{
-    empty_response, finalize_response, is_raft_request, is_write_request, WritableFlatBuffer,
-};
-use futures::future::{ok, result};
-use protobuf::Message as ProtobufMessage;
-use raft::eraftpb::Message;
+use crate::utils::{finalize_response, is_raft_request, is_write_request, WritableFlatBuffer};
 use std::net::SocketAddr;
 use std::path::Path;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 use tokio::timer::Interval;
-
-pub fn raft_message_handler<'a, 'b>(
-    request: GenericRequest<'a>,
-    raft_manager: &RaftManager,
-    mut builder: FlatBufferBuilder<'b>,
-) -> impl Future<Item = FlatBufferBuilder<'b>, Error = ErrorCode> {
-    let response: Box<
-        Future<
-                Item = (
-                    FlatBufferBuilder<'b>,
-                    ResponseType,
-                    WIPOffset<UnionWIPOffset>,
-                ),
-                Error = ErrorCode,
-            > + Send,
-    >;
-
-    match request.request_type() {
-        RequestType::FilesystemCheckRequest => unreachable!(),
-        RequestType::FilesystemChecksumRequest => unreachable!(),
-        RequestType::ReadRequest => unreachable!(),
-        RequestType::ReadRawRequest => unreachable!(),
-        RequestType::GetXattrRequest => unreachable!(),
-        RequestType::ListXattrsRequest => unreachable!(),
-        RequestType::SetXattrRequest => unreachable!(),
-        RequestType::RemoveXattrRequest => unreachable!(),
-        RequestType::AccessRequest => unreachable!(),
-        RequestType::HardlinkRequest => unreachable!(),
-        RequestType::RenameRequest => unreachable!(),
-        RequestType::ChmodRequest => unreachable!(),
-        RequestType::ChownRequest => unreachable!(),
-        RequestType::TruncateRequest => unreachable!(),
-        RequestType::FsyncRequest => unreachable!(),
-        RequestType::UnlinkRequest => unreachable!(),
-        RequestType::RmdirRequest => unreachable!(),
-        RequestType::WriteRequest => unreachable!(),
-        RequestType::UtimensRequest => unreachable!(),
-        RequestType::ReaddirRequest => unreachable!(),
-        RequestType::GetattrRequest => unreachable!(),
-        RequestType::MkdirRequest => unreachable!(),
-        RequestType::RaftRequest => {
-            let raft_request = request.request_as_raft_request().unwrap();
-            let mut deserialized_message = Message::new();
-            deserialized_message
-                .merge_from_bytes(raft_request.message())
-                .unwrap();
-            raft_manager
-                .apply_messages(&[deserialized_message])
-                .unwrap();
-            response = Box::new(result(empty_response(builder)));
-        }
-        RequestType::LatestCommitRequest => {
-            let index = raft_manager.get_latest_local_commit();
-            let mut response_builder = LatestCommitResponseBuilder::new(&mut builder);
-            response_builder.add_index(index);
-            let response_offset = response_builder.finish().as_union_value();
-            response = Box::new(ok((
-                builder,
-                ResponseType::LatestCommitResponse,
-                response_offset,
-            )));
-        }
-        RequestType::GetLeaderRequest => {
-            let leader_future = raft_manager
-                .get_leader()
-                .map(move |leader_id| {
-                    let mut response_builder = NodeIdResponseBuilder::new(&mut builder);
-                    response_builder.add_node_id(leader_id);
-                    let response_offset = response_builder.finish().as_union_value();
-                    (builder, ResponseType::NodeIdResponse, response_offset)
-                })
-                .map_err(|_| ErrorCode::Uncategorized);
-
-            response = Box::new(leader_future);
-        }
-        RequestType::NONE => unreachable!(),
-    }
-
-    response.map(|(mut builder, response_type, response_offset)| {
-        finalize_response(&mut builder, response_type, response_offset);
-        builder
-    })
-}
 
 #[derive(Clone)]
 pub struct LocalContext {
@@ -172,8 +85,7 @@ impl Node {
                         Future<Item = FlatBufferBuilder, Error = ErrorCode> + Send,
                     >;
                     if is_raft_request(request.request_type()) {
-                        builder_future =
-                            Box::new(raft_message_handler(request, &cloned_raft, builder));
+                        builder_future = Box::new(raft_handler(request, &cloned_raft, builder));
                     } else if is_write_request(request.request_type()) {
                         builder_future = Box::new(
                             cloned_raft
