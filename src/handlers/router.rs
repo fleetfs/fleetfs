930c51577ab1403ba7a4133de5c99cf1fa792350
Christopher Berner <christopherberner@gmail.com>
2019-06-25 22:09:56 -0700

    Refactor to make all read ops go through FileStorage

--- src/handlers/router.rs
+++ src/handlers/router.rs
@@ -1,23 +1,21 @@
 use crate::generated::*;
 use crate::handlers::fsck_handler::{checksum_request, fsck};
 use crate::storage::raft_manager::RaftManager;
-use crate::utils::{
-    finalize_response, into_error_code, to_inode_response, to_read_response, to_xattrs_response,
-};
+use crate::utils::finalize_response;
 use flatbuffers::{FlatBufferBuilder, UnionWIPOffset, WIPOffset};
-use futures::future::{err, result};
+use futures::future::result;
 use futures::Future;
 use std::sync::Arc;
 
-pub fn request_router<'a, 'b>(
-    request: GenericRequest<'a>,
+pub fn request_router(
+    request: GenericRequest,
     raft: Arc<RaftManager>,
-    builder: FlatBufferBuilder<'b>,
-) -> impl Future<Item = FlatBufferBuilder<'b>, Error = ErrorCode> {
+    builder: FlatBufferBuilder<'static>,
+) -> impl Future<Item = FlatBufferBuilder<'static>, Error = ErrorCode> {
     let response: Box<
         Future<
                 Item = (
-                    FlatBufferBuilder<'b>,
+                    FlatBufferBuilder<'static>,
                     ResponseType,
                     WIPOffset<UnionWIPOffset>,
                 ),
@@ -26,8 +24,6 @@ pub fn request_router<'a, 'b>(
     >;
     let context = raft.local_context();
     let file = raft.file_storage();
-    let data_storage = file.get_data_storage();
-    let metadata_storage = file.get_metadata_storage();
 
     match request.request_type() {
         RequestType::FilesystemCheckRequest => {
@@ -38,32 +34,22 @@ pub fn request_router<'a, 'b>(
         }
         RequestType::ReadRequest => {
             let read_request = request.request_as_read_request().unwrap();
-            match metadata_storage.read(read_request.inode(), *read_request.context()) {
-                Ok(_) => {
-                    let read_result = data_storage.read(
-                        read_request.inode(),
-                        read_request.offset(),
-                        read_request.read_size(),
-                    );
-                    response = Box::new(
-                        read_result.map(move |data| to_read_response(builder, &data).unwrap()),
-                    )
-                }
-                Err(error_code) => response = Box::new(err(error_code)),
-            }
-        }
-        RequestType::ReadRawRequest => {
-            let read_request = request.request_as_read_raw_request().unwrap();
-            let read_result = data_storage.read_raw(
+            response = Box::new(file.read(
                 read_request.inode(),
                 read_request.offset(),
                 read_request.read_size(),
-            );
-            response = Box::new(result(
-                read_result
-                    .map(move |data| to_read_response(builder, &data).unwrap())
-                    .map_err(into_error_code),
+                *read_request.context(),
+                builder,
             ));
+        }
+        RequestType::ReadRawRequest => {
+            let read_request = request.request_as_read_raw_request().unwrap();
+            response = Box::new(result(file.read_raw(
+                read_request.inode(),
+                read_request.offset(),
+                read_request.read_size(),
+                builder,
+            )));
         }
         RequestType::HardlinkRequest => unreachable!(),
         RequestType::RenameRequest => unreachable!(),
@@ -74,36 +60,27 @@ pub fn request_router<'a, 'b>(
         RequestType::CreateRequest => unreachable!(),
         RequestType::LookupRequest => {
             let lookup_request = request.request_as_lookup_request().unwrap();
-            // TODO: handle key doesn't exist
-            match metadata_storage.lookup(
+            response = Box::new(result(file.lookup(
                 lookup_request.parent(),
                 lookup_request.name(),
                 lookup_request.uid(),
                 lookup_request.gid(),
-            ) {
-                Ok(maybe_inode) => {
-                    if let Some(inode) = maybe_inode {
-                        response = Box::new(result(to_inode_response(builder, inode)));
-                    } else {
-                        response = Box::new(err(ErrorCode::DoesNotExist));
-                    }
-                }
-                Err(error_code) => response = Box::new(err(error_code)),
-            }
+                builder,
+            )));
         }
         RequestType::GetXattrRequest => {
             let get_xattr_request = request.request_as_get_xattr_request().unwrap();
-            // TODO: handle key doesn't exist
-            let data = metadata_storage
-                .get_xattr(get_xattr_request.inode(), get_xattr_request.key())
-                .unwrap_or_else(|| vec![]);
-            response = Box::new(result(to_read_response(builder, &data)));
+            response = Box::new(result(file.get_xattr(
+                get_xattr_request.inode(),
+                get_xattr_request.key(),
+                builder,
+            )));
         }
         RequestType::ListXattrsRequest => {
             let list_xattrs_request = request.request_as_list_xattrs_request().unwrap();
-            // TODO: handle key doesn't exist
-            let attrs = metadata_storage.list_xattrs(list_xattrs_request.inode());
-            response = Box::new(result(to_xattrs_response(builder, &attrs)));
+            response = Box::new(result(
+                file.list_xattrs(list_xattrs_request.inode(), builder),
+            ));
         }
         RequestType::SetXattrRequest => unreachable!(),
         RequestType::RemoveXattrRequest => unreachable!(),
