c0717420d5632f0b1184f757f23cf4f69d12b083
Christopher Berner <christopherberner@gmail.com>
2019-06-29 13:25:30 -0700

    Handle bad read requests

--- src/handlers/router.rs
+++ src/handlers/router.rs
@@ -3,7 +3,7 @@ use crate::handlers::fsck_handler::{checksum_request, fsck};
 use crate::storage::raft_manager::RaftManager;
 use crate::utils::{empty_response, finalize_response, FutureResultResponse};
 use flatbuffers::FlatBufferBuilder;
-use futures::future::{ok, result};
+use futures::future::{err, ok, result};
 use futures::Future;
 use protobuf::Message as ProtobufMessage;
 use raft::prelude::Message;
@@ -37,28 +37,34 @@ pub fn request_router(
             response = Box::new(result(checksum_request(raft.local_context(), builder)));
         }
         RequestType::ReadRequest => {
-            let read_request = request.request_as_read_request().unwrap();
-            let after_sync = sync_with_leader(&raft);
-            let inode = read_request.inode();
-            let offset = read_request.offset();
-            let read_size = read_request.read_size();
-            let user_context = *read_request.context();
-            let response_after_sync = after_sync
-                .map(move |_| {
-                    raft.file_storage()
-                        .read(inode, offset, read_size, user_context, builder)
-                })
-                .flatten();
-            response = Box::new(response_after_sync);
+            if let Some(read_request) = request.request_as_read_request() {
+                let after_sync = sync_with_leader(&raft);
+                let inode = read_request.inode();
+                let offset = read_request.offset();
+                let read_size = read_request.read_size();
+                let user_context = *read_request.context();
+                let response_after_sync = after_sync
+                    .map(move |_| {
+                        raft.file_storage()
+                            .read(inode, offset, read_size, user_context, builder)
+                    })
+                    .flatten();
+                response = Box::new(response_after_sync);
+            } else {
+                response = Box::new(err(ErrorCode::BadRequest));
+            }
         }
         RequestType::ReadRawRequest => {
-            let read_request = request.request_as_read_raw_request().unwrap();
-            response = Box::new(result(raft.file_storage().read_raw(
-                read_request.inode(),
-                read_request.offset(),
-                read_request.read_size(),
-                builder,
-            )));
+            if let Some(read_request) = request.request_as_read_raw_request() {
+                response = Box::new(result(raft.file_storage().read_raw(
+                    read_request.inode(),
+                    read_request.offset(),
+                    read_request.read_size(),
+                    builder,
+                )));
+            } else {
+                response = Box::new(err(ErrorCode::BadRequest));
+            }
         }
         RequestType::SetXattrRequest
         | RequestType::RemoveXattrRequest
@@ -77,64 +83,82 @@ pub fn request_router(
             response = Box::new(raft.propose(request, builder));
         }
         RequestType::LookupRequest => {
-            let lookup_request = request.request_as_lookup_request().unwrap();
-            let after_sync = sync_with_leader(&raft);
-            let parent = lookup_request.parent();
-            let name = lookup_request.name().to_string();
-            let user_context = *lookup_request.context();
-            let response_after_sync = after_sync
-                .map(move |_| {
-                    raft.file_storage()
-                        .lookup(parent, &name, user_context, builder)
-                })
-                .flatten();
-            response = Box::new(response_after_sync);
+            if let Some(lookup_request) = request.request_as_lookup_request() {
+                let after_sync = sync_with_leader(&raft);
+                let parent = lookup_request.parent();
+                let name = lookup_request.name().to_string();
+                let user_context = *lookup_request.context();
+                let response_after_sync = after_sync
+                    .map(move |_| {
+                        raft.file_storage()
+                            .lookup(parent, &name, user_context, builder)
+                    })
+                    .flatten();
+                response = Box::new(response_after_sync);
+            } else {
+                response = Box::new(err(ErrorCode::BadRequest));
+            }
         }
         RequestType::GetXattrRequest => {
-            let get_xattr_request = request.request_as_get_xattr_request().unwrap();
-            let after_sync = sync_with_leader(&raft);
-            let inode = get_xattr_request.inode();
-            let key = get_xattr_request.key().to_string();
-            let response_after_sync = after_sync
-                .map(move |_| raft.file_storage().get_xattr(inode, &key, builder))
-                .flatten();
-            response = Box::new(response_after_sync);
+            if let Some(get_xattr_request) = request.request_as_get_xattr_request() {
+                let after_sync = sync_with_leader(&raft);
+                let inode = get_xattr_request.inode();
+                let key = get_xattr_request.key().to_string();
+                let response_after_sync = after_sync
+                    .map(move |_| raft.file_storage().get_xattr(inode, &key, builder))
+                    .flatten();
+                response = Box::new(response_after_sync);
+            } else {
+                response = Box::new(err(ErrorCode::BadRequest));
+            }
         }
         RequestType::ListXattrsRequest => {
-            let list_xattrs_request = request.request_as_list_xattrs_request().unwrap();
-            let after_sync = sync_with_leader(&raft);
-            let inode = list_xattrs_request.inode();
-            let response_after_sync = after_sync
-                .map(move |_| raft.file_storage().list_xattrs(inode, builder))
-                .flatten();
-            response = Box::new(response_after_sync);
+            if let Some(list_xattrs_request) = request.request_as_list_xattrs_request() {
+                let after_sync = sync_with_leader(&raft);
+                let inode = list_xattrs_request.inode();
+                let response_after_sync = after_sync
+                    .map(move |_| raft.file_storage().list_xattrs(inode, builder))
+                    .flatten();
+                response = Box::new(response_after_sync);
+            } else {
+                response = Box::new(err(ErrorCode::BadRequest));
+            }
         }
         RequestType::ReaddirRequest => {
-            let readdir_request = request.request_as_readdir_request().unwrap();
-            let after_sync = sync_with_leader(&raft);
-            let inode = readdir_request.inode();
-            let response_after_sync = after_sync
-                .map(move |_| raft.file_storage().readdir(inode, builder))
-                .flatten();
-            response = Box::new(response_after_sync);
+            if let Some(readdir_request) = request.request_as_readdir_request() {
+                let after_sync = sync_with_leader(&raft);
+                let inode = readdir_request.inode();
+                let response_after_sync = after_sync
+                    .map(move |_| raft.file_storage().readdir(inode, builder))
+                    .flatten();
+                response = Box::new(response_after_sync);
+            } else {
+                response = Box::new(err(ErrorCode::BadRequest));
+            }
         }
         RequestType::GetattrRequest => {
-            let getattr_request = request.request_as_getattr_request().unwrap();
-            let after_sync = sync_with_leader(&raft);
-            let inode = getattr_request.inode();
-            let response_after_sync = after_sync
-                .map(move |_| raft.file_storage().getattr(inode, builder))
-                .flatten();
-            response = Box::new(response_after_sync);
+            if let Some(getattr_request) = request.request_as_getattr_request() {
+                let after_sync = sync_with_leader(&raft);
+                let inode = getattr_request.inode();
+                let response_after_sync = after_sync
+                    .map(move |_| raft.file_storage().getattr(inode, builder))
+                    .flatten();
+                response = Box::new(response_after_sync);
+            } else {
+                response = Box::new(err(ErrorCode::BadRequest));
+            }
         }
         RequestType::RaftRequest => {
-            let raft_request = request.request_as_raft_request().unwrap();
-            let mut deserialized_message = Message::new();
-            deserialized_message
-                .merge_from_bytes(raft_request.message())
-                .unwrap();
-            raft.apply_messages(&[deserialized_message]).unwrap();
-            response = Box::new(result(empty_response(builder)));
+            if let Some(raft_request) = request.request_as_raft_request() {
+                let mut deserialized_message = Message::new();
+                deserialized_message
+                    .merge_from_bytes(raft_request.message())
+                    .unwrap();
+                raft.apply_messages(&[deserialized_message]).unwrap();
+                response = Box::new(result(empty_response(builder)));
+            } else {
+                response = Box::new(err(ErrorCode::BadRequest));
+            }
         }
         RequestType::LatestCommitRequest => {
             let index = raft.get_latest_local_commit();
