f51bfe3654e91db1d89804d2b986a74269cad207
Christopher Berner <christopherberner@gmail.com>
2020-05-29 12:58:14 -0700

    Run pjdfs tests on a larger cluster

--- pjdfs.sh
+++ pjdfs.sh
@@ -12,14 +12,23 @@ export RUST_BACKTRACE=1
 
 DATA_DIR=$(mktemp --directory)
 DATA_DIR2=$(mktemp --directory)
+DATA_DIR3=$(mktemp --directory)
+DATA_DIR4=$(mktemp --directory)
+DATA_DIR5=$(mktemp --directory)
+DATA_DIR6=$(mktemp --directory)
 DIR=$(mktemp --directory)
 cargo build
-cargo run -- --port 3300 --data-dir $DATA_DIR --peers 127.0.0.1:3301 > /code/logs/daemon0.log 2>&1 &
-cargo run -- --port 3301 --data-dir $DATA_DIR2 --peers 127.0.0.1:3300 > /code/logs/daemon1.log 2>&1 &
+cargo run -- --port 3300 --data-dir $DATA_DIR --peers 127.0.0.1:3301,127.0.0.1:3302,127.0.0.1:3303,127.0.0.1:3304,127.0.0.1:3305 > /code/logs/daemon0.log 2>&1 &
+cargo run -- --port 3301 --data-dir $DATA_DIR2 --peers 127.0.0.1:3300,127.0.0.1:3302,127.0.0.1:3303,127.0.0.1:3304,127.0.0.1:3305 > /code/logs/daemon1.log 2>&1 &
+cargo run -- --port 3302 --data-dir $DATA_DIR3 --peers 127.0.0.1:3300,127.0.0.1:3301,127.0.0.1:3303,127.0.0.1:3304,127.0.0.1:3305 > /code/logs/daemon2.log 2>&1 &
+cargo run -- --port 3303 --data-dir $DATA_DIR4 --peers 127.0.0.1:3300,127.0.0.1:3301,127.0.0.1:3302,127.0.0.1:3304,127.0.0.1:3305 > /code/logs/daemon3.log 2>&1 &
+cargo run -- --port 3304 --data-dir $DATA_DIR5 --peers 127.0.0.1:3300,127.0.0.1:3301,127.0.0.1:3302,127.0.0.1:3303,127.0.0.1:3305 > /code/logs/daemon4.log 2>&1 &
+cargo run -- --port 3305 --data-dir $DATA_DIR6 --peers 127.0.0.1:3300,127.0.0.1:3301,127.0.0.1:3302,127.0.0.1:3303,127.0.0.1:3304 > /code/logs/daemon5.log 2>&1 &
 
 # Wait for leader to be elected
-sleep 0.5
-cargo run -- --server-ip-port 127.0.0.1:3300 --get-leader
+until cargo run --release -- --server-ip-port 127.0.0.1:3300 --get-leader; do
+    sleep 0.1
+done
 
 cargo run -- --server-ip-port 127.0.0.1:3300 --mount-point $DIR > /code/logs/mount.log 2>&1 &
 FUSE_PID=$!
@@ -38,6 +47,10 @@ cat /code/logs/pjdfs.log | egrep -o 'Failed: [0-9]+' | egrep -o '[0-9]+' | paste
 
 rm -rf ${DATA_DIR}
 rm -rf ${DATA_DIR2}
+rm -rf ${DATA_DIR3}
+rm -rf ${DATA_DIR4}
+rm -rf ${DATA_DIR5}
+rm -rf ${DATA_DIR6}
 
 kill $FUSE_PID
 wait $FUSE_PID
